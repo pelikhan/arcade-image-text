e83c749ab2d8f29f9f6ac7ad4b8707aaa509f56a
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-24 11:57:37 -0700

    adding text blocks

--- images.ts
+++ images.ts
@@ -1,20 +1,22 @@
 namespace images {
     /**
-     * Print some text on the image duh
+     * Print text on the image
      */
-    //% blockId=imagesprinttext block="print $i $text at y $y in color $color"
-    //% i.shadow=screen_image_picker
+    //% blockId=imagesprinttext block="print $i $text at x $x y $y in color $color"
+    //% i.shadow=variables_get
     //% color.shadow=colorindexpicker
+    //% group="Text"
     export function print(i: Image, text: string, x: number, y: number, color: number) {
         i.print(text, x, y, color)
     }
 
     /**
-     * Print some text on the image duh
+     * Print centered text on the image
      */
     //% blockId=imagesprinttextcenter block="print $i center $text at y $y in color $color"
-    //% i.shadow=screen_image_picker
+    //% i.shadow=variables_get
     //% color.shadow=colorindexpicker
+    //% group="Text"
     export function printCenter(i: Image, text: string, y: number, color: number) {
         i.printCenter(text, y, color)
     }
