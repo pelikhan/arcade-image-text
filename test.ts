362dc2e07371bdafa89f550ecc96262bd9d44384
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-25 12:29:32 -0700

    updated test

--- test.ts
+++ test.ts
@@ -1,17 +1,6 @@
 let i = image.create(80, 40)
-images.print(
-i,
-"hello",
-0,
-0,
-10
-)
-images.printCenter(
-i,
-"world",
-20,
-6
-)
+images.print(i, "hello", 0, 0, 10)
+images.printCenter(i, "hello", 20, 6)
 let mySprite = sprites.create(i, SpriteKind.Player)
 mySprite.setFlag(SpriteFlag.BounceOnWall, true)
 mySprite.vx = Math.randomRange(-200,200)
